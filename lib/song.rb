a5ab61dd16b08a5473c5f2d8377438ad1f0166e0
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 21:13:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,20 @@
 class Song
 
-artists = {}
-
   @@song_count = 0
+  @@genre_count = 0
 
   def self.count
     @@song_count
   end
 
-  def artists(artists)
+  def artists
     artists
   end
 
+  def genres
+    genres
+  end
+
+  def genre_count
+    @genre_count
 end
