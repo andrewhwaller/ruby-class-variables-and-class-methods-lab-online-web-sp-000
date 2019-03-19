3408d844398943c7424fb4c86d41515e6a2e750b
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 21:14:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
 
   @@song_count = 0
   @@genre_count = 0
+  @@artist_count = 0
 
   def self.count
     @@song_count
@@ -16,5 +17,11 @@ class Song
   end
 
   def genre_count
-    @genre_count
+    @@genre_count
+  end
+
+  def artist_count
+    @@artist_count
+  end
+  
 end
