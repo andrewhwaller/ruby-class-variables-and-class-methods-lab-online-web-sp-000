38175037f61cf1bc503346810ff54ca8c1d1f93d
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 21:37:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,14 +31,14 @@ class Song
   end
 
   def self.genre_count
-    genre_histo = {}
+    genre_count = {}
     @@genres.each do |genre|
-      if genre_histo[genre]
+      if genre_count[genre]
         genre_count += 1
       else
-        genre_histo[genre] = 1
+        genre_count[genre] = 1
       end
-      genre_histo
+      genre_count
     end
   end
 end
